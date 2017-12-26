2157331927c1ad1e22e7aec1f15f9c77dfffdb1a
Aaron Feldman <agf@fb.com>
2017-12-25 19:47:47 -0500

    Dropbox auth flow copypasta from dropbox example

--- app/src/main/java/com/ajax/diary_app/MainActivity.java
+++ app/src/main/java/com/ajax/diary_app/MainActivity.java
@@ -1,23 +1,127 @@
 package com.ajax.diary_app;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import com.dropbox.client2.DropboxAPI;
+import com.dropbox.client2.android.AndroidAuthSession;
+import com.dropbox.client2.android.AuthActivity;
+import com.dropbox.client2.session.AccessTokenPair;
+import com.dropbox.client2.session.AppKeyPair;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "DiaryAppMainActivity";
     public static final String EXTRA_MESSAGE = "com.ajax.diary_app.MESSAGE";
+
+    private static final String ACCOUNT_PREFS_NAME = "prefs";
+    private static final String ACCESS_KEY_NAME = "ACCESS_KEY";
+    private static final String ACCESS_SECRET_NAME = "ACCESS_SECRET";
+
+    private static final boolean USE_OAUTH1 = false;
+
+    DropboxAPI<AndroidAuthSession> mApi;
+
+    private boolean mLoggedIn;
+
+    // Android widgets
+    private Button mSubmit;
+    private LinearLayout mDisplay;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mApi = new DropboxAPI<>(buildSession());
+        checkAppKeySetup();
+
+        mSubmit = (Button)findViewById(R.id.auth_button);
+
+        mSubmit.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // This logs you out if you're logged in, or vice versa
+                if (mLoggedIn) {
+                    logOut();
+                } else {
+                    // Start the remote authentication
+                    if (USE_OAUTH1) {
+                        mApi.getSession().startAuthentication(MainActivity.this);
+                    } else {
+                        mApi.getSession().startOAuth2Authentication(MainActivity.this);
+                    }
+                }
+            }
+        });
+
+        mDisplay = (LinearLayout)findViewById(R.id.logged_in_display);
+
+        // Display the proper UI state if logged in or not
+        setLoggedIn(mApi.getSession().isLinked());
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        AndroidAuthSession session = mApi.getSession();
+
+        // The next part must be inserted in the onResume() method of the
+        // activity from which session.startAuthentication() was called, so
+        // that Dropbox authentication completes properly.
+        if (session.authenticationSuccessful()) {
+            try {
+                // Mandatory call to complete the auth
+                session.finishAuthentication();
+
+                // Store it locally in our app for later use
+                storeAuth(session);
+                setLoggedIn(true);
+            } catch (IllegalStateException e) {
+                showToast("Couldn't authenticate with Dropbox:" + e.getLocalizedMessage());
+                Log.i(TAG, "Error authenticating", e);
+            }
+        }
+    }
+
+    /**
+     * Shows keeping the access keys returned from Trusted Authenticator in a local
+     * store, rather than storing user name & password, and re-authenticating each
+     * time (which is not to be done, ever).
+     */
+    private void storeAuth(AndroidAuthSession session) {
+        // Store the OAuth 2 access token, if there is one.
+        String oauth2AccessToken = session.getOAuth2AccessToken();
+        if (oauth2AccessToken != null) {
+            SharedPreferences prefs = getSharedPreferences(ACCOUNT_PREFS_NAME, 0);
+            SharedPreferences.Editor edit = prefs.edit();
+            edit.putString(ACCESS_KEY_NAME, "oauth2:");
+            edit.putString(ACCESS_SECRET_NAME, oauth2AccessToken);
+            edit.commit();
+            return;
+        }
+        // Store the OAuth 1 access token, if there is one.  This is only necessary if
+        // you're still using OAuth 1.
+        AccessTokenPair oauth1AccessToken = session.getAccessTokenPair();
+        if (oauth1AccessToken != null) {
+            SharedPreferences prefs = getSharedPreferences(ACCOUNT_PREFS_NAME, 0);
+            SharedPreferences.Editor edit = prefs.edit();
+            edit.putString(ACCESS_KEY_NAME, oauth1AccessToken.key);
+            edit.putString(ACCESS_SECRET_NAME, oauth1AccessToken.secret);
+            edit.commit();
+            return;
+        }
     }
 
     @Override
@@ -58,5 +162,81 @@ public class MainActivity extends AppCompatActivity {
             return true;
         }
         return false;
+    }
+
+    private void loadAuth(AndroidAuthSession session) {
+        SharedPreferences prefs = getSharedPreferences(ACCOUNT_PREFS_NAME, 0);
+        String key = prefs.getString(ACCESS_KEY_NAME, null);
+        String secret = prefs.getString(ACCESS_SECRET_NAME, null);
+        if (key == null || secret == null || key.length() == 0 || secret.length() == 0) return;
+
+        if (key.equals("oauth2:")) {
+            // If the key is set to "oauth2:", then we can assume the token is for OAuth 2.
+            session.setOAuth2AccessToken(secret);
+        } else {
+            // Still support using old OAuth 1 tokens.
+            session.setAccessTokenPair(new AccessTokenPair(key, secret));
+        }
+    }
+
+    private AndroidAuthSession buildSession() {
+        AppKeyPair appKeyPair = new AppKeyPair(BuildConfig.DROPBOX_APP_KEY, BuildConfig.DROPBOX_APP_SECRET);
+
+        AndroidAuthSession session = new AndroidAuthSession(appKeyPair);
+        loadAuth(session);
+        return session;
+    }
+
+    private void checkAppKeySetup() {
+        // TODO(agf)
+        // Check if the app has set up its manifest properly.
+        Intent testIntent = new Intent(Intent.ACTION_VIEW);
+        String scheme = "db-" + BuildConfig.DROPBOX_APP_KEY;
+        String uri = scheme + "://" + AuthActivity.AUTH_VERSION + "/test";
+        testIntent.setData(Uri.parse(uri));
+        PackageManager pm = getPackageManager();
+        if (0 == pm.queryIntentActivities(testIntent, 0).size()) {
+            showToast("URL scheme in your app's " +
+                    "manifest is not set up correctly. You should have a " +
+                    "com.dropbox.client2.android.AuthActivity with the " +
+                    "scheme: " + scheme);
+            finish();
+        }
+    }
+
+    private void showToast(String msg) {
+        Toast error = Toast.makeText(this, msg, Toast.LENGTH_LONG);
+        error.show();
+    }
+
+    private void logOut() {
+        // Remove credentials from the session
+        mApi.getSession().unlink();
+
+        // Clear our stored keys
+        clearKeys();
+        // Change UI state to display logged out version
+        setLoggedIn(false);
+    }
+
+    /**
+     * Convenience function to change UI state based on being logged in
+     */
+    private void setLoggedIn(boolean loggedIn) {
+        mLoggedIn = loggedIn;
+        if (loggedIn) {
+            mSubmit.setText("Unlink from Dropbox");
+            mDisplay.setVisibility(View.VISIBLE);
+        } else {
+            mSubmit.setText("Link with Dropbox");
+            mDisplay.setVisibility(View.GONE);
+        }
+    }
+
+    private void clearKeys() {
+        SharedPreferences prefs = getSharedPreferences(ACCOUNT_PREFS_NAME, 0);
+        SharedPreferences.Editor edit = prefs.edit();
+        edit.clear();
+        edit.commit();
     }
 }
